e68a11fda7490ccc46e6f1aef4c93163cb359932
ashok4822 <4822ashok@gmail.com>
2024-03-28 16:17:50 +0530

    product display css pending 20645

--- src/Components/ProductDisplay/ProductDisplay.jsx
+++ src/Components/ProductDisplay/ProductDisplay.jsx
@@ -35,7 +35,7 @@ function ProductDisplay(props) {
                 <div className='productdisplay-right-price-new'>${product.new_price}</div>
             </div>
             <div className='productdisplay-right-description'>
-                A lightweight, usually knitted,
+                A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.
             </div>
             <div className='productdisplay-right-size'>
                 <h1>Select Size</h1>
