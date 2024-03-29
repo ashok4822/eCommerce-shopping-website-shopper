496d2da4e4428fff2adb7ef88cccd41d23d0d2e0
ashok4822 <4822ashok@gmail.com>
2024-03-29 23:44:12 +0530

    media query of navbar for 800px completed 31550

--- src/Components/CartItems/CartItems.jsx
+++ src/Components/CartItems/CartItems.jsx
@@ -5,7 +5,7 @@ import remove_icon from '../Assets/cart_cross_icon.png';
 
 function CartItems() {
 
-    const {all_product, cartItems, removeFromCart} = useContext(ShopContext);
+    const {getTotalCartAmount, all_product, cartItems, removeFromCart} = useContext(ShopContext);
 
   return (
     <div className='cartitems'>
@@ -40,7 +40,7 @@ function CartItems() {
                 <div>
                     <div className='cartitems-total-item'>
                         <p>Subtotal</p>
-                        <p>${0}</p>
+                        <p>${getTotalCartAmount()}</p>
                     </div>
                     <hr />
                     <div className='cartitems-total-item'>
@@ -50,7 +50,7 @@ function CartItems() {
                     <hr />
                     <div className='cartitems-total-item'>
                         <h3>Total</h3>
-                        <h3>${0}</h3>
+                        <h3>${getTotalCartAmount()}</h3>
                     </div>
                 </div>
                 <button>PROCEED TO CHECKOUT</button>
